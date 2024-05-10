2a74fa836f7ad8c20977b30e7aa389beb150c7d0
einfachFriedrich <registrierrp@gmail.com>
2024-05-10 09:10:36 +0200

    added instructions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+//run app
 void main() {
   runApp(const MyApp());
 }
@@ -34,16 +35,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //initial counter value
   int _counter = 0;
+  //setup methodChannel
   final channel = MethodChannel('com.rectify.watch');
 
+  //gets called after the button in the ContentView is pressed
   Future<void> _incrementCounter() async {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+      //add 1 to the counter variable
       _counter++;
     });
 
@@ -52,14 +52,19 @@ class _MyHomePageState extends State<MyHomePage> {
         "flutterToWatch", {"method": "sendCounterToNative", "data": _counter});
   }
 
+
+  // Function for recieving Data from Native
   Future<void> _initFlutterChannel() async {
     /*await*/ channel.setMethodCallHandler((call) async {
-      // Receive data from Native
+      //check which way the data has to go
       switch (call.method) {
         case "sendCounterToFlutter":
           _counter = call.arguments["data"]["counter"];
           _incrementCounter();
+          //exit func
           break;
+
+        //do nothing if it's shouldn't be recieved (declared on the iOS Side)
         default:
           break;
       }
@@ -69,49 +74,25 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
+    //calls methodChannel for recieving Data from Native
     _initFlutterChannel();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
             ),
+            //Display Counter
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
@@ -119,6 +100,8 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+
+      //button that calls "_incrementCounter" on press
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
