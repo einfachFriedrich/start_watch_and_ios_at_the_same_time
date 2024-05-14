13f7127652c5e2f6f82e41c7db41fabef2c7ce72
einfachFriedrich <registrierrp@gmail.com>
2024-05-14 14:32:42 +0200

    local vibrates value

--- ios/watchapp Watch App/ContentView.swift
+++ ios/watchapp Watch App/ContentView.swift
@@ -9,19 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var viewModel: WatchViewModel = WatchViewModel()
+    @State private var vibrates = false
    
     var body: some View {
         VStack {
             Text("Counter: \(viewModel.counter)")
                 .padding()
             Button(action: {
-                viewModel.sendDataMessage(for: .syncRequest, data: ["counter": viewModel.counter])
-                   // viewModel.sendDataMessage(for: .sendDataToFlutter, data: ["counter": viewModel.counter])
+                //viewModel.sendDataMessage(for: .syncRequest, data: ["counter": viewModel.counter])
+                   viewModel.sendDataMessage(for: .sendDataToFlutter, data: ["counter": viewModel.counter])
             }) {
                 Text("+ by 1")
             }
-            Toggle("Vibrates", isOn: $viewModel.vibrates)
-                .onChange(of: viewModel.vibrates) { oldValue, newValue in
+            Toggle("Vibrates", isOn: $vibrates)
+                .onChange(of: vibrates) { oldValue, newValue in
                     viewModel.sendDataMessage(for: .vibratesToFlutter, data: ["vibrates": viewModel.vibrates])
                 }
         }
